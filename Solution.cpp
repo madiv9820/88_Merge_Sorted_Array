bfe6fcdda39518a6a77b0875d7c27d52866bc0ba
madiv9820 <themathur901@gmail.com>
2024-10-04 18:03:13 +0530

    C++ Solution

--- Solution.cpp
+++ Solution.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 class Solution {
@@ -9,32 +10,22 @@ class Solution {
                     vector<int>& nums2,  // Second sorted array
                     int n) {            // Number of elements in nums2
             
-            // Create a temporary vector to hold the merged elements
-            vector<int> temp_Vector(m + n, 0); 
-            int ptr1 = 0, ptr2 = 0, index = 0; // Initialize pointers for both arrays and the index for temp_Vector
-
-            // Merge the two arrays until one is exhausted
-            while (ptr1 < m && ptr2 < n) {
-                // Compare elements from both arrays and add the smaller one to temp_Vector
-                if (nums1[ptr1] <= nums2[ptr2]) 
-                    temp_Vector[index++] = nums1[ptr1++]; // Add from nums1 and move pointer
-                else 
-                    temp_Vector[index++] = nums2[ptr2++]; // Add from nums2 and move pointer
-            }
-
-            // If there are remaining elements in nums1, add them to temp_Vector
-            while (ptr1 < m) 
-                temp_Vector[index++] = nums1[ptr1++]; // Add remaining elements from nums1
+            // Append elements from nums2 to the end of nums1
+            for(int index = 0; index < n; ++index)
+                nums1[m + index] = nums2[index];  // Fill nums1 with elements from nums2
             
-            // If there are remaining elements in nums2, add them to temp_Vector
-            while (ptr2 < n) 
-                temp_Vector[index++] = nums2[ptr2++]; // Add remaining elements from nums2
-
-            // Copy the sorted elements back into nums1
-            for (index = 0; index < m + n; ++index) 
-                nums1[index] = temp_Vector[index]; // Update nums1 with merged results
+            // Sort the merged array to ensure all elements are in non-decreasing order
+            sort(nums1.begin(), nums1.end());  // Sort the entire nums1 array
         }
 };
+
+// Time Complexity: O((m + n) log(m + n)), where m is the number of elements in nums1 and n is the number of elements in nums2.
+// This is due to the sorting operation on the combined array of size (m + n).
+
+// Space Complexity: O(n), where n is the number of elements in nums2, as we are using additional space 
+// for sorting the combined array. The effective space used for merging can be considered as O(m + n), 
+// but auxiliary space is dominated by the sorting process.
+
 
 int main() {
     vector<int> nums1 = {1,2,3,0,0,0}, nums2 = {2,5,6};
